b4d3172ae2775773946ef5f19c9b99294696a507
calculusdiv68 <calculusdiv68@gmail.com>
2021-11-29 08:18:32 -0800

    fixes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -13,6 +13,6 @@ COLLATE utf8mb4_unicode_ci;
 -- Convert field 'name' in 'first_table'
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CHANGE name  VARCHAR(256)
+CHANGE name name  VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
